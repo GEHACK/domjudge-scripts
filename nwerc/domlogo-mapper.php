fa8759da7329e9ba5ee296051d2d9c039ef471e7
Nicky Gerritsen <nickygerritsen@me.com>
2018-11-18 18:26:38 +0100

    Add images for domlogo and script to copy them from ccsconfig

---
+++ nwerc/domlogo-mapper.php
@@ -0,0 +1,38 @@
+<?php
+
+$teams = explode("\n", file_get_contents($argv[1]));
+$mapping = [];
+foreach ($teams as $idx => $teamData) {
+    if ($idx === 0)
+    {
+        continue;
+    }
+    $teamData = str_replace("\r", "", $teamData);
+    IF (empty($teamData))
+    {
+        continue;
+    }
+    $teamData = explode("\t", $teamData);
+
+    $mapping[str_replace('INST-', '', $teamData[7])] = $teamData[0];
+}
+
+$sourceDir = $argv[2];
+$domlogoDir = $argv[3];
+
+if ($handle = opendir($sourceDir))
+{
+    while (($entry = readdir($handle)) !== false)
+    {
+        if (preg_match('/^U-\d+$/', $entry)) {
+            if (!isset($mapping[$entry])) {
+                echo "${entry} NOT FOUND!\n";
+            } else {
+                $newName = sprintf('%s/%s.png', $domlogoDir, $mapping[$entry]);
+                copy($sourceDir . '/' . $entry . '/logo-512.png', $newName);
+            }
+        }
+    }
+
+    closedir($handle);
+}
